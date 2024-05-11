291ee3a2c18d27d18d8d44d8921dd91683d62e6e
Bekzod <bekzodisakov18@gmail.com>
2024-05-11 18:15:33 +0500

    version-3(single product)

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const container = document.querySelector(".container");
 init();
 
 async function init() {
-  checkToken()
+  checkToken();
 
   const products = await fetchProducts(); // array
   render(products);
@@ -14,14 +14,14 @@ function checkToken() {
 
   // no token
   if (!token) {
-    window.location.href = "http://127.0.0.1:5500/login.html"
+    window.location.href = "http://127.0.0.1:5500/login.html";
   }
 }
 
 function showSpinner() {
   const div = document.createElement("div");
   div.className = "spinner";
-  document.body.prepend(div)
+  document.body.prepend(div);
 }
 
 function hideSpinner() {
@@ -31,37 +31,42 @@ function hideSpinner() {
 
 async function fetchProducts() {
   showSpinner();
-  
+
   const response = await fetch("https://fakestoreapi.com/products");
   const products = await response.json();
-  
+
   hideSpinner();
   return products;
 }
 
 function render(products) {
-  products.forEach(function(product) { // object
+  products.forEach(function (product) {
+    // object
     const li = document.createElement("li");
-   
+
+    const a = document.createElement("a");
+    a.href = `http://127.0.0.1:5500/product.html?id=${product.id}`;
+
     const img = document.createElement("img");
     img.src = product.image;
     li.append(img);
-    
+
     const title = document.createElement("p");
     title.textContent = product.title;
-    li.append(title);
-    
+    a.append(title);
+    li.append(a);
+
     const price = document.createElement("strong");
     price.textContent = product.price;
     li.append(price);
-    
+
     const stars = "<span>⭐️</span>".repeat(Math.round(product.rating.rate));
     li.insertAdjacentHTML("beforeend", stars);
-    
+
     const ratingCount = document.createElement("div");
     ratingCount.textContent = `(${product.rating.count})`;
     li.append(ratingCount);
-    
+
     container.append(li);
-  })
+  });
 }
